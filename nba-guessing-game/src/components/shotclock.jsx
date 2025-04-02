089b18cf6b85ee48a33a7a40f56b396eb9255bbe
Cameron Anderson <cammando@hotmail.com.au>
2025-04-02 23:14:52 +1100

    Add styling to scoreboard to look similar to NBA shot clock

--- nba-guessing-game/src/components/shotclock.jsx
+++ nba-guessing-game/src/components/shotclock.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 function ShotClock ({correctGuesses, incorrectGuesses, guessCount}) {
     return (
-        <div className='shot-clock'>
-            <p>{correctGuesses} : {incorrectGuesses}</p>
-            <p>{guessCount}</p>
+        <div className='shot-clock-scoreboard'>
+            <h3>Game Scoreboard</h3>
+
+            <div className="shot-clock">
+                <p><span className="shot-clock-correct">{correctGuesses}</span>:<span className="shot-clock-incorrect">{incorrectGuesses}</span></p>
+                <p className="shot-clock-counter">{guessCount}</p>
+            </div>
         </div>
     )
 }
