42824b67aa3f2cba8821d70764e4e2c9ac92ea70
Cameron Anderson <cammando@hotmail.com.au>
2025-04-22 18:27:22 +1000

    Add array of team codes with matching team name, and function to replace the team code from allPlayerData API response with team name in array

--- nba-guessing-game/src/App.jsx
+++ nba-guessing-game/src/App.jsx
@@ -21,6 +21,159 @@ function App() {
   const [randPlayerIndex, setRandPlayerIndex] = useState(Math.floor(Math.random()*filteredPlayerData.length))
   const [guessResultHistory, setGuessResultHistory] = useState([]);
 
+  const teamNames = [
+    {
+      id: 0,
+      teamCode: 'ATL',
+      teamName: 'Atlanta Hawks'
+    },
+    {
+      id: 1,
+      teamCode: 'BOS',
+      teamName: 'Boston Celtics'
+    },
+    {
+      id: 2,
+      teamCode: 'BRK',
+      teamName: 'Brooklyn Nets'
+    },
+    {
+      id: 3,
+      teamCode: 'CHI',
+      teamName: 'Chicago Bulls'
+    },
+    {
+      id: 4,
+      teamCode: 'CHO',
+      teamName: 'Charlotte Hornets'
+    },
+    {
+      id: 5,
+      teamCode: 'CLE',
+      teamName: 'Cleveland Cavaliers'
+    },
+    {
+      id: 6,
+      teamCode: 'DAL',
+      teamName: 'Dallas Mavericks'
+    },
+    {
+      id: 7,
+      teamCode: 'DEN',
+      teamName: 'Denver Nuggets'
+    },
+    {
+      id: 8,
+      teamCode: 'DET',
+      teamName: 'Detroit Pistons'
+    },
+    {
+      id: 9,
+      teamCode: 'GSW',
+      teamName: 'Golden State Warriors'
+    },
+    {
+      id: 10,
+      teamCode: 'HOU',
+      teamName: 'Houston Rockets'
+    },
+    {
+      id: 11,
+      teamCode: 'IND',
+      teamName: 'Indiana Pacers'
+    },
+    {
+      id:12,
+      teamCode: 'LAC',
+      teamName: 'Los Angeles Clippers'
+    },
+    {
+      id: 13,
+      teamCode: 'LAL',
+      teamName: 'Los Angeles Lakers'
+    },
+    {
+      id: 14,
+      teamCode: 'MEM',
+      teamName: 'Memphis Grizzlies'
+    },
+    {
+      id: 15,
+      teamCode: 'MIA',
+      teamName: 'Miami Heat'
+    },
+    {
+      id: 16,
+      teamCode: 'MIL',
+      teamName: 'Milwaukee Bucks'
+    },
+    {
+      id: 17,
+      teamCode: 'MIN',
+      teamName: 'Minnesota Timberwolves'
+    },
+    {
+      id: 18,
+      teamCode: 'NOP',
+      teamName: 'New Orleans Pelicans'
+    },
+    {
+      id: 19,
+      teamCode: 'NYK',
+      teamName: 'New York Knicks'
+    },
+    {
+      id: 20,
+      teamCode: 'OKC',
+      teamName: 'Oklahoma City Thunder'
+    },
+    {
+      id: 21,
+      teamCode: 'ORL',
+      teamName: 'Orlando Magic'
+    },
+    {
+      id: 22,
+      teamCode: 'PHI',
+      teamName: 'Philadelphia 76ers'
+    },
+    {
+      id: 23,
+      teamCode: 'PHO',
+      teamName: 'Phoenix Suns'
+    },
+    {
+      id: 24,
+      teamCode: 'POR',
+      teamName: 'Portland Trail Blazers'
+    },
+    {
+      id: 25,
+      teamCode: 'SAC',
+      teamName: 'Sacramento Kings'
+    },
+    {
+      id: 26,
+      teamCode: 'SAS',
+      teamName: 'San Antonio Spurs'
+    },
+    {
+      id: 27,
+      teamCode: 'TOR',
+      teamName: 'Toronto Raptors'
+    },
+    {
+      id: 28,
+      teamCode: 'UTA',
+      teamName: 'Utah Jazz'
+    },
+    {
+      id: 29,
+      teamCode: 'WAS',
+      teamName: 'Washington Wizards'
+    },
+  ]
+
   //State update functions to pass into Game component
   const updateCorrectCount = (newCount) => setCorrect(newCount);
   const updateIncorrectCount = (newCount) => setIncorrect(newCount);
@@ -55,6 +208,14 @@ function App() {
     return Object.values(uniquePlayers);
   }
 
+  function checkTeamCode(teamCode) {
+    const matchTeamCode = teamNames.find(team => (
+      team.teamCode === teamCode
+    ));
+
+    return matchTeamCode ? matchTeamCode.teamName : teamCode;
+  }
+
   //Function to collect list of player data (team, stats, ID)
   useEffect(() => {
     async function fillPlayerDict() {
@@ -72,7 +233,7 @@ function App() {
           return {
             playerId: player.id,
             playerName: player.playerName,
-            playerTeam: player.team,
+            playerTeam: checkTeamCode(player.team),
             playerGuessedYet: false,
             playerPpg: Math.round((player.points/player.games)*100)/100,
             playerRpg: Math.round((player.totalRb/player.games)*100)/100,
